3cb647ffe8920a6cf14a78d892464fab4bde1d0c
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-08-02 22:27:12 +0530

    updated

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 const startServer = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
     );
   } catch (error) {
     console.error(error);
